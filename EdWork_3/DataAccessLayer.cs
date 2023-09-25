4ca05b77b4922a14b911d5f481b593fcfcb08913
Nataly Zakharova <naataliaa0804@yandex.ru>
2023-09-25 12:15:38 +0300

    Linq project

---
+++ EdWork_3/DataAccessLayer.cs
@@ -0,0 +1,155 @@
+﻿using NewVariant.Interfaces;
+using NewVariant.Models;
+using NewVariant.Exceptions;
+using System;
+using System.Security.Cryptography;
+
+namespace DataLib
+{
+    public class DataAccessLayer : IDataAccessLayer
+    {
+        public DataAccessLayer() { }
+        /// <summary>
+        /// Метод возвращает список всех товаров, купленных покупателем с самым
+        /// длинным именем.
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public IEnumerable<Good> GetAllGoodsOfLongestNameBuyer(IDataBase dataBase)
+        {
+            var result = from good in dataBase.GetTable<Good>()
+                         where ( from sale in dataBase.GetTable<Sale>()
+                         where sale.BuyerId == ( from  buyers in dataBase.GetTable<Buyer>()
+                                                 orderby buyers.Name.Length,buyers.Name
+                                                 select buyers.Id).LastOrDefault()
+                                 select sale.GoodId).Contains(good.Id)
+                                 select good;
+            return result;
+        }
+        /// <summary>
+        /// Метод возвращает название категории самого дорого товара.
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public string GetMostExpensiveGoodCategory(IDataBase dataBase)
+        {
+            var goods = dataBase.GetTable<Good>()
+                .OrderByDescending(g => g.Price)
+                .ToList();
+            if (goods.Count == 0)
+            {
+                return null;
+            }
+            return goods
+                .First()
+                .Category;
+        }
+        /// <summary>
+        /// Метод получает название города, в котором было потрачено меньше всего денег.
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public string GetMinimumSalesCity(IDataBase dataBase)
+        {
+            var shops = dataBase.GetTable<Shop>().OrderBy(s => s.City).ToList();
+            List<Sale> sales = dataBase.GetTable<Sale>().ToList();
+            Dictionary<string, long> cities = new Dictionary<string, long>();
+            foreach (var shop in shops)
+            {
+                long sum = sales
+                    .Where(sale => shop.Id == sale.ShopId)
+                    .Sum(sale => sale.GoodCount);
+                cities[shop.City] = sum;
+            }
+
+            if (cities.Count == 0)
+            {
+                return null;
+            }
+            return cities
+                .OrderBy(x => x.Value)
+                .First()
+                .Key;
+        }
+        /// <summary>
+        /// Метод возвращает список покупателей, которые купили самый популярный товар.
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public IEnumerable<Buyer> GetMostPopularGoodBuyers(IDataBase dataBase)
+        {
+            var goodsb = new Dictionary<Good, long>();
+            foreach (var good in dataBase.GetTable<Good>())
+            {
+                long count = dataBase.GetTable<Sale>()
+                    .Where(sale => sale.GoodId == good.Id)
+                    .Sum(sale => sale.GoodCount);
+                goodsb[good] = count;
+            }
+            var gooods = goodsb.OrderByDescending(g => g.Value);
+            if (gooods.Count() == 0)
+            {
+                return new List<Buyer>();
+            }
+            var g = gooods
+                .First()
+                .Key;
+            var buyersbb = (from sale in dataBase.GetTable<Sale>()
+                            where sale.GoodId == g.Id
+                            from b in dataBase.GetTable<Buyer>()
+                            where sale.BuyerId == b.Id
+                            select b).ToList();
+            return buyersbb;
+        }
+        /// <summary>
+        /// Для каждой страны метод определяет количество ее магазинов. 
+        /// Возвращает наименьшее из полученных значений.
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public int GetMinimumNumberOfShopsInCountry(IDataBase dataBase)
+        {
+            var answer = (from s in (from s in dataBase.GetTable<Shop>()
+                                     group s by s.Country)
+                          orderby s.Count()
+                          select s.Count());
+            if (dataBase.GetTable<Shop>().Count() == 0)
+            {
+                return 0;
+            }
+            return answer.FirstOrDefault();
+        }
+        /// <summary>
+        ///  Метод возвращает список покупок, совершенных покупателями во всех городах, кроме города проживания. 
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public IEnumerable<Sale> GetOtherCitySales(IDataBase dataBase)
+        {
+            var sales = dataBase.GetTable<Sale>();
+            var shops = dataBase.GetTable<Shop>();
+            var buyers = dataBase.GetTable<Buyer>();
+            var answer = (from sale in sales
+                          join buy in buyers on sale.BuyerId equals buy.Id
+                          join shop in shops on sale.ShopId equals shop.Id
+                          where shop.City != buy.City
+                          select sale);
+            return answer;
+        }
+        /// <summary>
+        ///  Метод возвращает общую стоимость покупок.
+        /// </summary>
+        /// <param name="dataBase"></param>
+        /// <returns></returns>
+        public long GetTotalSalesValue(IDataBase dataBase)
+        {
+            var sales = dataBase.GetTable<Sale>();
+            var goods = dataBase.GetTable<Good>();
+            return (from sale in sales
+                    from good in goods
+                    where sale.GoodId == good.Id
+                    select good.Price * sale.GoodCount)
+                .Sum();
+        }
+    }
+}
---
+++ EdWork_3/DataBase.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NewVariant.Exceptions;
+using NewVariant.Interfaces;
+using NewVariant.Models;
+
+
+    public class DataBase : IDataBase
+    {
+        private Dictionary<Type, List<IEntity>> dk = new Dictionary<Type, List<IEntity>>();
+        public DataBase() { }
+    /// <summary>
+    /// Метод создает новую таблицу типа Т.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <exception cref="DataBaseException"> Исключение, выбрасываемое если уже таблица сущесвует.</exception>
+
+    public void CreateTable<T>() where T : IEntity
+        {
+        try
+        {
+            if (dk.ContainsKey(typeof(T)))
+            {
+                throw new DataBaseException();
+            }
+            else
+            {
+                dk.Add(typeof(T), new List<IEntity>());
+            }
+
+        }
+        catch
+        {
+
+            throw new DataBaseException();
+        }
+
+     }
+    /// <summary>
+    ///  Метод возвращает ссылку на таблицу типа Т. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="DataBaseException"> Исключение, выбрасываемое если таблица уже существует. </exception>
+
+    public IEnumerable<T> GetTable<T>() where T : IEntity
+        {    
+            if (!dk.ContainsKey(typeof(T)))
+            {
+                throw new DataBaseException();
+            }
+            if (typeof(T) == typeof(Buyer))
+            {
+                List<Buyer> bBuyers = new List<Buyer>();
+                foreach (var b in dk[typeof(T)])
+                {
+                    bBuyers.Add((Buyer)b);
+                }
+                return (IEnumerable<T>)bBuyers;
+            }
+            if (typeof(T) == typeof(Shop))
+            {
+                List<Shop> sShops = new List<Shop>();
+                foreach (var b in dk[typeof(T)])
+                {
+                    sShops.Add((Shop)b);
+                }
+                return (IEnumerable<T>)sShops;
+            }
+            if (typeof(T) == typeof(Sale))
+            {
+                List<Sale> sSales = new List<Sale>();
+                foreach (var b in dk[typeof(T)])
+                {
+                    sSales.Add((Sale)b);
+                }
+                return (IEnumerable<T>)sSales;
+            }
+            if (typeof(T) == typeof(Good))
+            {
+                List<Good> gGoods = new List<Good>();
+                foreach (var b in dk[typeof(T)])
+                {
+                    gGoods.Add((Good)b);
+                }
+                return (IEnumerable<T>)gGoods;
+            }
+
+            throw new DataBaseException();
+        }
+    /// <summary>
+    /// Метод добавляет новую строку в таблицу типа Т.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="getEntity">Возвращает результат соответсвующего типа.</param>
+    /// <exception cref="DataBaseException"></exception>
+
+    public void InsertInto<T>(Func<T> getEntity) where T : IEntity
+        {
+        try
+        {
+            if (!dk.ContainsKey(typeof(T)))
+            {
+                throw new DataBaseException();
+            }
+            dk[typeof(T)].Add(getEntity.Invoke());
+
+        }
+        catch
+        {
+            throw new DataBaseException();
+        }        
+     }
+    /// <summary>
+    /// Метод сериализует таблицу типа Т.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path">Путь файла.</param>
+    /// <exception cref="DataBaseException"></exception>
+    public void Serialize<T>(string path) where T : IEntity
+     {
+        try
+        { 
+            if (!dk.ContainsKey(typeof(T)))
+            {
+                throw new DataBaseException();
+            }
+            else
+            {
+                List<T> newList = new List<T>();
+                if (dk[typeof(T)] != null)
+                {
+                    foreach (IEntity obj in dk[typeof(T)])
+                    {
+                        newList.Add((T)obj);
+                    }
+                }
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    JsonSerializer.Serialize(fs,newList);
+                }
+            }
+
+        }
+        catch 
+        {
+
+            throw new DataBaseException();
+        }         
+     }
+    /// <summary>
+    /// Метод десериализует и сохраняет таблицу типа Т из файла.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path">Путь файла.</param>
+    public void Deserialize<T>(string path) where T : IEntity
+    {
+        try
+        { 
+            if (File.Exists(path))
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                List<T> inputList = JsonSerializer.Deserialize(fs, typeof(List<T>)) as List<T>;
+                List<IEntity> newList = new List<IEntity>();
+                if (inputList.Count > 0)
+                {
+                    foreach (T obj in inputList)
+                    { 
+                        newList.Add(obj);
+                    }
+                    if (dk.ContainsKey(typeof(T)))
+                    {
+                        dk[typeof(T)] = newList;
+                    }
+                    else
+                    {
+                        dk.Add(typeof(T), newList);
+                    }
+                }
+            }
+        }
+
+        }
+        catch
+        {
+            throw new DataBaseException();
+        }      
+    }
+}
+       
+
